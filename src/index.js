5a74c959087152335861611f6cc592cf16f0a1a1
Daniel Martinez <daniel.martinez@mckesson.com>
2022-03-22 16:31:13 -0600

    Add faker-js library and add our first avatar image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { faker } from "@faker-js/faker";
+
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()} />
         </a>
         <div className="content">
           <a href="/" className="author">
