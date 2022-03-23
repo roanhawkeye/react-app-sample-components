f544526d9c9dbc54befdb78801129c3a0b12d6dc
Daniel Martinez <daniel.martinez@mckesson.com>
2022-03-23 09:41:45 -0600

    Add Approval card component and pass Comment Detail as a props.children

--- src/index.js
+++ src/index.js
@@ -2,30 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 import { faker } from "@faker-js/faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        avatar={faker.image.avatar()}
-        content="Nice blog post!"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00AM"
-        avatar={faker.image.avatar()}
-        content="Some nice blog"
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yestarday at 5:00PM"
-        avatar={faker.image.avatar()}
-        content="No comments"
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          avatar={faker.image.avatar()}
+          content="Nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          avatar={faker.image.avatar()}
+          content="Some nice blog"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yestarday at 5:00PM"
+          avatar={faker.image.avatar()}
+          content="No comments"
+        />
+      </ApprovalCard>
     </div>
   );
 };
